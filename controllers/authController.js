3ae2f55322f26ea36ca2e30c4c64b883285b843a
Zak Ahmed <zakahmed2006@hotmail.com>
2025-07-17 14:28:19 +0100

    sendgrid

--- controllers/authController.js
+++ controllers/authController.js
@@ -166,11 +166,8 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   await user.save({ validateBeforeSave: false });
 
   // 3) Send to user's email
-  const resetURL = `${req.protocol}://${req.get('host')}/api/v1/users/resetPassword/${resetToken}`;
-
-  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you did't forget your password, please ignore this email!`;
-
   try {
+    const resetURL = `${req.protocol}://${req.get('host')}/api/v1/users/resetPassword/${resetToken}`;
     await new Email(user, resetURL).sendPasswordReset();
 
     res.status(200).json({
